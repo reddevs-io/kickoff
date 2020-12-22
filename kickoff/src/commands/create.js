937a6019cb52fa9da6d7b2b151d66b093fff87d9
Francis Makokha <francismakokha@gmail.com>
2020-12-22 23:10:06 +0300

    feat: Added docker-compose.yml generate from selected options

--- kickoff/src/commands/create.js
+++ kickoff/src/commands/create.js
@@ -5,6 +5,7 @@ const chalk  = require('chalk')
 const fs = require('fs')
 const ncp = require('ncp').ncp
 const path = require('path')
+const yaml = require('js-yaml')
 
 class CreateCommand extends Command {
   cleanAppName(appName) {
@@ -22,7 +23,12 @@ class CreateCommand extends Command {
                         `))
   }
 
-  generateProjectFiles(appName, appType) {
+  generateProjectFiles(userInput) {
+    let appName = userInput.name
+    let appType = userInput.type
+    // let phpVersion = userInput.php
+    let additionalServices = userInput.services
+
     let folderName = this.cleanAppName(appName)
     let templateFolder = path.join(__dirname, '/templates/', appType)
 
@@ -31,13 +37,122 @@ class CreateCommand extends Command {
         return this.log("There was a problem creating your project's folder: " + chalk.red(err.toString()))
       }
       this.log('Folder - ' + chalk.bold(folderName) + ' -  ' + chalk.green('successfully created!'))
+      // Copy nginx & PHP ini files
       ncp(templateFolder, path.join(folderName, '/.kickoff'), err => {
         if (err) {
           return this.log('There was a problem while copying your files: ' + chalk.red(err))
         }
-        this.printSuccessMessage(appType, folderName)
       })
     })
+
+    let dockerCompose = this.getDockerComposeJson(folderName, additionalServices)
+
+    fs.writeFile(folderName + '/docker-compose.yml', yaml.safeDump(dockerCompose), 'utf8', err => {
+      if (err) {
+        return this.log('There was a problem while generating the docker-compose.yaml file: ' + chalk.red(err))
+      }
+    })
+  }
+
+  getDockerComposeJson(appName, additionalServices) {
+    let dockerCompose = {
+      version: '3',
+      services: {
+        nginx: {
+          image: 'nginx:1.18-alpine',
+          // eslint-disable-next-line camelcase
+          container_name: appName + '_nginx',
+          labels: [
+            'traefik.enable=true',
+            'traefik.http.services.nginx.loadbalancer.server.port=80',
+            'traefik.http.routers.nginx.rule=Host(`' + appName + '.kickoff.test`)',
+            'traefik.http.routers.nginx.tls=true',
+          ],
+          expose: [
+            '80',
+          ],
+          networks: [
+            'kickoff-proxy',
+          ],
+          volumes: [
+            '.kickoff/nginx/nginx.conf:/etc/nginx/nginx.conf',
+            '.kickoff/nginx/servers:/etc/nginx/conf.d',
+            './.logs/nginx:/var/log/nginx',
+            '.:/app:ro',
+          ],
+          // eslint-disable-next-line camelcase
+          depends_on: [
+            'php',
+          ],
+          restart: 'always',
+        },
+        php: {
+          build: {
+            context: './.kickoff/php',
+          },
+          // eslint-disable-next-line camelcase
+          container_name: appName + '_php',
+          environment: {
+            XDEBUG_CONFIG: 'remote_host=host.docker.internal',
+          },
+          volumes: [
+            '.:/app',
+            '.kickoff/php/dev.ini:/usr/local/etc/php/conf.d/dev.ini',
+          ],
+          restart: 'always',
+        },
+      },
+      networks: {
+        'kickoff-proxy': {
+          external: {
+            name: 'kickoff-proxy',
+          },
+        },
+      },
+    }
+
+    if (additionalServices.includes('database')) {
+      dockerCompose.services.database = {
+        image: 'mariadb:10.5',
+        // eslint-disable-next-line camelcase
+        container_name: appName + '_database',
+        environment: [
+          'MYSQL_ROOT_PASSWORD=lamp',
+          'MYSQL_DATABASE=lamp',
+        ],
+        expose: [
+          '3306',
+        ],
+        restart: 'always',
+      }
+      // eslint-disable-next-line camelcase
+      dockerCompose.services.php.depends_on = [
+        'database',
+      ]
+    }
+
+    if (additionalServices.includes('mail')) {
+      dockerCompose.services.mail = {
+        image: 'mailhog/mailhog',
+        // eslint-disable-next-line camelcase
+        container_name: appName + '_mailhog',
+        labels: [
+          'traefik.enable=true',
+          'traefik.http.services.mailhog.loadbalancer.server.port=8025',
+          'traefik.http.routers.mailhog.rule=Host(`' + appName + '-mail.kickoff.test`)',
+          'traefik.http.routers.mailhog.tls=true',
+        ],
+        expose: [
+          '8025',
+        ],
+        networks: [
+          'kickoff-proxy',
+        ],
+        restart: 'always',
+      }
+    }
+
+    return dockerCompose
   }
 
   async run() {
@@ -49,13 +164,25 @@ class CreateCommand extends Command {
         choices: ['symfony', 'drupal'],
       },
       {
+        type: 'list',
+        name: 'php',
+        message: 'PHP version:',
+        choices: ['7.3', '7.4', '8'],
+      },
+      {
         type: 'input',
         name: 'name',
         message: 'Application name:',
       },
+      {
+        type: 'checkbox',
+        name: 'services',
+        message: 'Additional services:',
+        choices: ['database', 'mail'],
+      },
     ])
 
-    this.generateProjectFiles(userInput.name, userInput.type)
+    this.generateProjectFiles(userInput)
   }
 }
 
